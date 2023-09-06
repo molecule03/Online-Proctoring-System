b1e967b2f045a17923a02e1bda2f94a990eafe3e
molecule03 <laveshsalaskar619@gmail.com>
2023-09-06 20:12:21 +0530

    Initial commit

---
+++ src/run.py
@@ -0,0 +1,19 @@
+import audio
+import head_pose
+import detection
+import threading as th
+
+# Just create a enviorment install all dependensies and run this file
+if __name__ == "__main__":
+    # main()
+    head_pose_thread = th.Thread(target=head_pose.pose)
+    audio_thread = th.Thread(target=audio.sound)
+    detection_thread = th.Thread(target=detection.run_detection)
+
+    head_pose_thread.start()
+    audio_thread.start()
+    detection_thread.start()
+
+    head_pose_thread.join()
+    audio_thread.join()
+    detection_thread.join()
